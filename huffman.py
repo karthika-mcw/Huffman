feb707ef828a207fba8b7188d2dab4043e434420
karthika_mcw <karthikadevi@multicorewareinc.com>
2020-10-24 23:47:54 +0530

    Enable binary write and read in huff file that reduces file size

--- huffman.py
+++ huffman.py
@@ -31,17 +31,16 @@ def set_binary(self):
 			binary_code[self.right.char] = self.right.binary
 		set_binary(self.right)
 
-def write_encode_info(binary_code,output_file):
-	fs = open(output_file,'w+')
-	count = len(binary_code)*2+1
-	fs.write(str(count+1)) #separate encodeded information from encoded text
-	fs.write("\n")
-	for i in binary_code:
-		fs.write(binary_code[i])
-		fs.write("\n")
-		fs.write(i)
-		fs.write("\n")
-	fs.close()
+def binaryToChar(binary_string):
+	n = int('0b'+binary_string, 2)
+	c = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+	return c
+
+def strToBytes(data):
+	b = bytearray()
+	for i in range(0, len(data), 8):
+		b.append(int(data[i:i+8], 2))
+	return bytes(b)
 
 def get_frequency(input_file):
 	frequency= {}
@@ -76,12 +75,111 @@ def form_tree(priority_queue):
 		second = priority_queue.pop()
 	return first
 
-def append_encoded_content(input_file,output_file,binary_code):
-	fo = open(output_file,'a+')
-	with open(input_file) as f:
-		for line in f:
-			for word in line:
-				fo.write(binary_code[word])	#write the encoded value of each character
+def write_encode_info(binary_code,output_file):
+	fs = open(output_file,'wb')
+	count = 0
+	for i in binary_code:
+		if (binary_code[i] != ''):
+			count += 1
+	fs.write(count.to_bytes(1, 'little'))
+	dot = '.'
+	for i in binary_code:
+		if (binary_code[i] != ''):
+			fs.write(binary_code[i].encode())	#write encoded value of binary value
+			fs.write(dot.encode())
+			fs.write(i.encode())	#write encoded value of binary key
+			fs.write(dot.encode())
+	return fs
+
+def append_encoded_content(fo,input_file,output_file,binary_code):
+	fi = open(input_file,'r')
+	input_file_content = fi.read()	#read the input text file
+	input_encoded = ''.join([binary_code[literal] for literal in input_file_content])	#combine codes of all literals of file
+	bit_count = len(input_encoded)	#nuumber of bits in encode
+	mod8 = bit_count%8
+	pad_length = 8 - mod8	#number of bits needed to make bit_count as multiple of 8
+	if pad_length == 8:
+			pad_length = 0
+	zero_length = f'{bin(pad_length)[2:]:0>8}'	#binary representation of pad length
+	pad_zeroes = '0' * pad_length	#replicate zeroes for padding
+	input_encoded = input_encoded + pad_zeroes	#add padding to actual content
+	input_encoded = zero_length + input_encoded		#add pad length to beginning of encoded_content
+	fo.write(strToBytes(input_encoded))
+	fo.close()
+
+def get_decode_info(input_file):
+	fi = open(input_file, 'rb')
+	file_content = ''
+	byte = fi.read(1)
+	while len(byte) :
+		file_content += f"{bin(ord(byte))[2:]:0>8}"
+		byte = fi.read(1)
+	margin = file_content[:8]	#first byte denotes the margin
+	margin = int(margin, base=2)
+	index = 8	#start after margin
+	dot_count = 0
+	break_point = 2*margin	#breakpoint refers the point to stop reading encoded info
+	prev_dot = 0
+	second_dot = 0	#previous and second dot are used to separate delimiter from actual dot 
+	key = 1		#represents if next search (key or value)
+	val = ''
+	binary_decode = {}
+	while 1:
+		char = binaryToChar(file_content[index:(index+8)])
+		index += 8
+		if (char == '.'):
+			dot_count += 1
+			if(key == 1):	#reverses the search
+				key = 0
+			else:
+				key = 1
+			if(prev_dot == 1):	#special case of having dot as key 
+				second_dot = 1
+				key_ = '.'
+				binary_decode[val] = key_
+				val = ''
+				key = 0
+				prev_dot = 0
+				second_dot = 0
+			else:
+				prev_dot = 1
+			if (dot_count > break_point):
+				break
+		else:	#found key
+			prev_dot = 0
+			if(key == 0):
+				key_ = char
+				binary_decode[val] = key_
+				val = ''
+			else:	#append value till delimiter occurs
+				val += char
+	return index,binary_decode
+
+def write_output(input_file,output_file,binary_decode,index):
+	fi = open(input_file, 'rb')
+	file_content = ''
+	byte = fi.read(1)
+	while len(byte) > 0:
+		file_content += f"{bin(ord(byte))[2:]:0>8}"	#get input file content
+		byte = fi.read(1)
+	binary_pad = file_content[index:index+8]	#get zero pad length in first byte
+	fo = open(output_file,'w+')
+	input_data = file_content[(index+8):]	#store from next byte
+	padded_val = int(binary_pad, base=2)
+	if padded_val > 1:
+		input_data = input_data[:-padded_val+1]	#remove the padded content
+	start_ind = 0 	#start point of substring match
+	end_ind = 1 	#end point of substring match
+	len_ = len(input_data)
+	decoded_content = ''
+	while end_ind < len_:
+		sub_str = input_data[start_ind:end_ind]
+		if sub_str in binary_decode.keys():
+			decoded_content += binary_decode[sub_str]
+			start_ind = end_ind		#key found write the value to file and start reading from next byte
+		end_ind += 1
+	# writing decoded text to a text file
+	fo.write(decoded_content)
 	fo.close()
 
 def encode(input_file, output_file):
@@ -90,49 +188,10 @@ def encode(input_file, output_file):
 	frequency =OrderedDict(a)
 	priority_queue = get_priority_queue(frequency)
 	priority_queue.reverse()
-	root = form_tree(priority_queue)
-	set_binary(root)
-	write_encode_info(binary_code,output_file)
-	append_encoded_content(input_file,output_file,binary_code)
-
-def write_output(input_file,output_file,binary_decode,margin):
-	fo = open(output_file,'w+')
-	current_word = ""
-	itr = 0
-	with open(input_file,'r') as f:
-		for line in f:
-			if (itr > margin-1):
-				for word in line:
-					current_word = current_word+word	#incremental read and addition of updating current_word
-					if current_word in binary_decode.keys():	#check if current_word is in binary keys
-						fo.write(binary_decode[current_word])	#write the decoded character
-						current_word = ""	#start looking for next match
-			itr+=1
-	fo.close()
-
-def get_decode_info(input_file):
-	file1 = open(input_file, 'r')
-	Lines = file1.readlines()
-	decoders = []
-	itr=0
-	binary_decode={}
-	for line in Lines:
-		decoders.append(line[:-1])
-	key = ""
-	margin = int(Lines[0]) #Margin is to separate encodeded information from encoded text
-	for i in decoders:
-		if ( itr < margin):
-			if (itr<4):
-				if itr==1:
-					binary_decode[i] = "\n" #new line character decode info
-			elif(itr%2==0):
-				key = i
-			else:
-				binary_decode[key] = i #collect the encode encryption info of characters other that '\n'
-			itr+=1
-		else:
-			break
-	return margin,binary_decode
+	root = form_tree(priority_queue)	#form tree from priority queue
+	set_binary(root)	#set binary for all nodes in tree
+	fs = write_encode_info(binary_code,output_file)
+	append_encoded_content(fs,input_file,output_file,binary_code)
 
 def decode(input_file, output_file):
 	margin,binary_decode = get_decode_info(input_file)
